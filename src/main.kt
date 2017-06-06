17813cb59201420dcfe9d49962d0964d06d6fdb8
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-06-06 08:49:56 -0500

    Using ranges

---
+++ src/main.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by carlosjoseanguiano on 06/06/17.
+ */
+
+fun main(args: Array<String>) {
+    val x = 10
+    val y = 9
+    if (x in 1..y + 1) {
+        println("fits in range")
+    }
+}
